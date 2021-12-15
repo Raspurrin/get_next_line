a8235b25824b6003be0fb788ee6b3320c2fc68b8
Raspurrin <vondetou@gmail.com>
2021-12-15 10:32:07 +0100

    Chopped into three functions in total (excluding utils)

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 21:14:26 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/15 10:25:31 by mialbert         ###   ########.fr       */
+/*   Updated: 2021/12/15 10:30:05 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,29 @@ static void	*ft_free(char *buf)
 	return (NULL);
 }
 
+static char	*ft_trim(char *remainder, char *line, size_t buflen)
+{
+	size_t	i;
+
+	i = 0;
+	while (line[i] != '\n')
+		i++;
+	buflen = i + 1;
+	while (line[buflen])
+		buflen++;
+	if ((buflen - i - 1) > 0)
+		remainder = ft_substr(line, i + 1, (i + buflen - 1));
+	line = ft_substr(line, 0, i + 1);
+	return (remainder);
+}
+
 char	*get_next_line(int fd)
 {
-	size_t		i;
 	size_t		buflen;
 	char		*line;
 	char		*buf;
 	static char	*remainder;
 
-	i = 0;
 	if (remainder)
 		line = remainder;
 	else
@@ -45,14 +59,7 @@ char	*get_next_line(int fd)
 		buf[buflen] = '\0';
 		line = ft_strjoin(line, buf);
 	}
-	while (line[i] != '\n')
-		i++;
-	buflen = i + 1;
-	while (line[buflen])
-		buflen++;
-	if ((buflen - i - 1) > 0)
-		remainder = ft_substr(line, i + 1, (i + buflen - 1));
-	line = ft_substr(line, 0, i + 1);
+	remainder = ft_trim(remainder, line, buflen);
 	return (line);
 }
 
@@ -68,6 +75,6 @@ int	main(void)
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
+	// printf("%s", get_next_line(fd));
 	return (0);
 }
