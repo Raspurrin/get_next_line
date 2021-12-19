7eefba4e637c02c7a9c96a2c2e5c522ca36448af
Raspurrin <vondetou@gmail.com>
2021-12-19 18:19:13 +0100

    Latest version.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 21:01:25 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/17 22:01:56 by mialbert         ###   ########.fr       */
+/*   Updated: 2021/12/19 18:17:38 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,16 @@
 
 char	*get_next_line(int fd)
 {
-	static char	*remainder;
+	static char	*remainder = "";
 	char		*line;
 	char		*buf;
-	size_t		buflen;
+	ssize_t		buflen;
 	size_t		linelen;
 
 	linelen = 0;
-	if (remainder)
-		line = remainder;
-	else
-		line = "";
-	buf = malloc(BUF_SIZE * sizeof(char));
+	line = remainder;
+	buflen = 1;
+	buf = ft_calloc(BUF_SIZE + 1, sizeof(char));
 	if (!buf || fd < 0)
 	{
 		free(buf);
@@ -36,49 +34,67 @@ char	*get_next_line(int fd)
 	}
 	if (!(ft_strchr(line, '\n')))
 	{
-		while (!(ft_strchr(buf, '\n')))
+		while (!(ft_strchr(buf, '\n') && buf[buflen]))
 		{
 			buflen = read(fd, buf, BUF_SIZE);
+			if (!buf)
+			{
+				free (buf);
+				if (remainder)
+					free (remainder);
+				return (NULL);
+			}
 			if (buflen == 0)
 			{
 				free (buf);
-				free (remainder);
-				return (line);
+				if (line)
+					return (line);
+				return (NULL);
 			}
-			if (buflen <= 0)
+			if (buflen < 0)
 			{
 				free (buf);
-				free (remainder);
 				return (NULL);
 			}
 			buf[buflen] = '\0';
 			line = ft_strjoin(line, buf);
 		}
 	}
+	if (buf == NULL)
+	{
+		free (buf);
+		if (remainder)
+			free (remainder);
+		return (NULL);
+	}
 	while (line[linelen] != '\n')
 		linelen++;
-	linelen++;
-	buflen = (ft_strlen(line) - linelen);
-	remainder = ft_substr(line, linelen, buflen + 1);
-	line = ft_substr(line, 0, linelen + 1);
+	if (line[linelen] == '\n')
+	{
+		linelen++;
+		buflen = (ft_strlen(line) - linelen);
+		remainder = ft_substr(line, linelen, buflen + 1);
+		line = ft_substr(line, 0, linelen + 1);
+	}
 	return (line);
 }
 
-int	main(void)
-{
-	int		fd;
+// int	main(void)
+// {
+// 	int		fd;
 
-	fd = open("test.txt", 0);
-	if (!fd)
-		return (0);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	return (0);
-}
+// 	fd = open("test.txt", 0);
+// 	if (!fd)
+// 		return (0);
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	close(fd);
+// 	return (0);
+// }
