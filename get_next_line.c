fba823dd7f4810f7ebe68873bd3adb93202f6254
Raspurrin <vondetou@gmail.com>
2021-12-22 22:08:10 +0100

    Merged remainder with buf. Works, but still memory leaks.

--- get_next_line.c
+++ get_next_line.c
@@ -1,64 +1,53 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/20 15:43:34 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/21 20:56:37 by mialbert         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 // #include "get_next_line_utils.c"
+
+// merge remainder and buf to avoid allocation of memory for buf
+// memset buf while reading
+// freeing in strjoin and such
 
 #include <stdio.h>
 
 char	*get_next_line(int fd)
 {
-	static char	*remainder = "";
 	char		*line;
-	char		*buf;
+	static char	buf[BUF_SIZE + 1];
 	ssize_t		buflen;
-	size_t		remlen;
 	size_t		linelen;
 
 	linelen = 0;
-	line = remainder;
-	buf = ft_calloc(BUF_SIZE + 1, sizeof(char));
-	if (!buf || fd < 0)
+	// buf = ft_calloc(BUF_SIZE + 1, sizeof(char));
+	
+	if (fd < 0)
 	{
-		free(buf);
 		return (NULL);
 	}
-	if (!(ft_strchr(line, '\n')))
+	line = "";
+	if (buf[0] != '\0')
+		line = ft_strjoin(line, buf);
+	while (!(ft_strchr(buf, '\n')))
 	{
-		while (!(ft_strchr(buf, '\n')))
+		ft_memset(buf, '\0', BUF_SIZE);
+		buflen = read(fd, buf, BUF_SIZE);
+		// if (!buf)
+		// {
+		// 	free (buf);
+		// 	if (remainder)
+		// 		free (remainder);
+		// 	return (NULL);
+		// }
+		if (buflen <= 0)
 		{
-			buflen = read(fd, buf, BUF_SIZE);
-			// if (!buf)
-			// {
-			// 	free (buf);
-			// 	if (remainder)
-			// 		free (remainder);
-			// 	return (NULL);
-			// }
-			if (buflen <= 0)
-			{
-				free (buf);
-				return (NULL);
-			}
-			if (buflen < BUF_SIZE)
-			{
-				line = ft_strjoin(line, buf);
-				// free (buf);
-				return (line);
-			}
-
-			buf[buflen] = '\0';
-			line = ft_strjoin(line, buf);
+			// free (buf);
+			return (NULL);
 		}
+		if (buflen < BUF_SIZE)
+		{
+			// free (buf);
+			line = ft_strjoin(line, buf);
+			return (line);
+		}
+		buf[buflen] = '\0';
+		line = ft_strjoin(line, buf);
 	}
 	// if (buf == NULL)
 	// {
@@ -70,33 +59,34 @@ char	*get_next_line(int fd)
 	while (line[linelen] != '\n')
 		linelen++;
 	linelen++;
-	remlen = (ft_strlen(line) - linelen);
-	// if (buflen != 0)
-	remainder = ft_substr(line, linelen, remlen + 1);
+	buflen = (ft_strlen(line) - linelen);
+	if (buflen != 0)
+		ft_strlcpy(buf, line + linelen, buflen + 1);
+	else
+		ft_memset(buf, '\0', BUF_SIZE);
+	// ft_substr(line, linelen, remlen + 1);
 	line = ft_substr(line, 0, linelen + 1);
 	return (line);
 }
 
-// int	main(void)
-// {
-// 	int		fd;
+int	main(void)
+{
+	int		fd;
 
-// 	fd = open("test.txt", 0);
-// 	if (!fd)
-// 		return (0);
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	close(fd);
-// 	return (0);
-// }
+	fd = open("test.txt", 0);
+	if (!fd)
+		return (0);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	close(fd);
+	return (0);
+}
