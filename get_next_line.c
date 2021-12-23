a13044b15299009faf625eb72f2b95702b9d5b3a
Raspurrin <vondetou@gmail.com>
2021-12-23 16:18:09 +0100

    Just in case

--- get_next_line.c
+++ get_next_line.c
@@ -1,11 +1,10 @@
 #include "get_next_line.h"
 // #include "get_next_line_utils.c"
 
-// merge remainder and buf to avoid allocation of memory for buf
-// memset buf while reading
-// freeing in strjoin and such
-
 #include <stdio.h>
+
+// make sure that I can free line in strjoin and before
+// returning line if buflen < BUF_SIZE
 
 char	*get_next_line(int fd)
 {
@@ -15,9 +14,10 @@ char	*get_next_line(int fd)
 	size_t		linelen;
 
 	linelen = 0;
-	line = "";
+	
 	if (buf[0] != '\0')
 		line = ft_strjoin(line, buf);
+		
 	while (!(ft_strchr(buf, '\n')))
 	{
 		ft_memset(buf, '\0', BUF_SIZE);
@@ -45,15 +45,27 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-// int	main(void)
-// {
-// 	int		fd;
+int	main(void)
+{
+	int		fd[8];
+	int		i;
+	char	*line;
 
-// 	fd = open("test.txt", 0);
-// 	if (!fd)
-// 		return (0);
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	close(fd);
-// 	return (0);
-// }
+	i = 4;
+	fd[0] = open("41_no_nl", 0);
+	fd[1] = open("41_with_nl", 0);
+	fd[2] = open("42_no_nl", 0);
+	fd[3] = open("42_with_nl", 0);
+	fd[4] = open("43_no_nl", 0);
+	fd[5] = open("43_with_nl", 0);
+	fd[6] = open("alternate_line_nl_no_nl", 0);
+	fd[7] = open("alternate_line_nl_with_nl", 0);
+	while (i < 5)
+	{
+		line = get_next_line(fd[i]);
+		printf("%s", line);
+		if (!line)
+			i++;
+	}
+	return (0);
+}
