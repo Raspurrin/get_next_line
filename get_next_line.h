a97e912fc2cfcb2a43348312abd2d1a1ff0f6f56
Raspurrin <vondetou@gmail.com>
2021-12-23 23:11:16 +0100

    Added more functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 21:34:14 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/14 21:18:37 by mialbert         ###   ########.fr       */
+/*   Updated: 2021/12/23 17:07:48 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 
-# define BUF_SIZE 5
+# define BUF_SIZE 42
 
 /**
  * @brief 
@@ -30,11 +30,15 @@
 char	*get_next_line(int fd);
 int		ft_strlen(const char *str);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-char	*ft_strdup(const char *str);
+char	*ft_strdup(char *str);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 char	*ft_strchr(const char *str, int32_t c);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-char	*ft_strjoin(char const *str1, char const *str2);
-char	*ft_substr(char const *str, uint32_t start, size_t len);
+char	*ft_strjoin(char *str1, char *str2);
+char	*ft_substr(char *str, uint32_t start, size_t len);
+void	*ft_calloc(size_t num, size_t size);
+void	ft_bzero(void *src, size_t n);
+void	*ft_memset(void *src, int32_t c, size_t n);
+size_t	ft_strlcpy(char *dst, char *src, size_t size);
 
 #endif
