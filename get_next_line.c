27c5209095b805c404d48a27610c1837c8c9d715
Raspurrin <vondetou@gmail.com>
2021-12-27 20:36:08 +0100

    Changed extra function names from ft to gnl.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 21:42:31 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/27 18:41:46 by mialbert         ###   ########.fr       */
+/*   Updated: 2021/12/27 20:35:06 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,27 @@
 
 #include <stdio.h>
 
+int	gnl_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * @brief Trims everything after a newline char into the remainder 
+ * and the rest into line.
+ * example: line = "Something is here\\nAnd"
+ * Result:
+ * line = "Something is here\\n"
+ * remainder = "And"
+ * @param line ex: "Something is here\\nAnd"
+ * @param remainder This will store the remainder of the line.
+ * @return The line up till and including the newline char.
+ */
 static char	*ft_trim(char *line, char *remainder)
 {
 	ssize_t		buflen;
@@ -25,20 +46,29 @@ static char	*ft_trim(char *line, char *remainder)
 		linelen++;
 	if (line[linelen] == '\n')
 		linelen++;
-	buflen = (ft_strlen(line) - linelen);
+	buflen = (gnl_strlen(line) - linelen);
 	if (buflen != 0)
-		ft_strlcpy(remainder, line + linelen, buflen + 1);
+		gnl_strlcpy(remainder, line + linelen, buflen + 1);
 	else
 		remainder[0] = '\0';
-	line = ft_substr(line, 0, linelen + 1);
+	line = gnl_substr(line, 0, linelen + 1);
 	return (line);
 }
 
-char	*read_line(char	*line, char *buf, int fd)
+/**
+ * @brief Reads the full buffersize until finding a newline char
+ * and appends this to line each time.
+ * @param line Will hold all characters being read in this function.
+ * @param buf Holds buffersize amount of chars every single read.
+ * @param fd The file descriptor being read from.
+ * @return A line including a newline character and possibly characters
+ * afterwards or NULL if nothing has been read.
+ */
+static char	*read_line(char	*line, char *buf, int fd)
 {
 	ssize_t		buflen;
 
-	while (!(ft_strchr(buf, '\n')))
+	while (!(gnl_strchr(buf, '\n')))
 	{
 		buflen = read(fd, buf, BUF_SIZE);
 		buf[buflen] = '\0';
@@ -47,8 +77,8 @@ char	*read_line(char	*line, char *buf, int fd)
 			free (line);
 			return (NULL);
 		}
-		line = ft_strjoin(line, buf);
-		if (buflen < BUF_SIZE && !(ft_strchr(buf, '\n')))
+		line = gnl_strjoin(line, buf);
+		if (buflen < BUF_SIZE && !(gnl_strchr(buf, '\n')))
 			return (line);
 	}
 	return (line);
@@ -59,11 +89,12 @@ char	*get_next_line(int fd)
 	char		*line;
 	static char	buf[BUF_SIZE + 1];
 
-	line = ft_calloc(1, 1);
+	line = malloc(1 * sizeof(char));
+	line[0] = '\0';
 	if (!line)
 		return (NULL);
 	if (buf[0] != '\0')
-		line = ft_strjoin(line, buf);
+		line = gnl_strjoin(line, buf);
 	line = read_line(line, buf, fd);
 	if (!line)
 		return (NULL);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,28 +6,18 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 19:56:40 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/25 14:12:01 by mialbert         ###   ########.fr       */
+/*   Updated: 2021/12/27 20:35:06 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-size_t	ft_strlcpy(char *dst, char *src, size_t size)
+size_t	gnl_strlcpy(char *dst, char *src, size_t size)
 {
 	size_t	i;
 	size_t	len;
 
-	len = ft_strlen((char *)src);
+	len = gnl_strlen((char *)src);
 	i = 0;
 	if (size == 0)
 		return (len);
@@ -41,30 +31,21 @@ size_t	ft_strlcpy(char *dst, char *src, size_t size)
 	return (len);
 }
 
-char	*ft_strdup(char *str)
+char	*gnl_strdup(char *str)
 {
 	size_t	len;
 	char	*out;
 
-	len = ft_strlen((char *)str) + 1;
+	len = gnl_strlen((char *)str) + 1;
 	out = malloc(len * sizeof(char));
 	if (!out)
 		return (NULL);
-	ft_strlcpy(out, str, len + 1);
+	gnl_strlcpy(out, str, len + 1);
 	free (str);
 	return (out);
 }
 
-void	ft_bzero(void *src, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-		((uint8_t *)src)[i++] = '\0';
-}
-
-char	*ft_strchr(const char *str, int32_t c)
+char	*gnl_strchr(const char *str, int32_t c)
 {
 	char	*str2;
 
@@ -80,52 +61,41 @@ char	*ft_strchr(const char *str, int32_t c)
 	return (0);
 }
 
-void	*ft_calloc(size_t num, size_t size)
-{
-	void	*ptr;
-
-	ptr = malloc(num * size);
-	if (!ptr)
-		return (NULL);
-	ft_bzero(ptr, num * size);
-	return (ptr);
-}
-
-char	*ft_strjoin(char *str1, char *str2)
+char	*gnl_strjoin(char *str1, char *str2)
 {
 	size_t	str1len;
 	size_t	str2len;
 	char	*out;
 
-	if (!str1)
-		return (ft_strdup(str2));
-	str1len = ft_strlen(str1);
-	str2len = ft_strlen(str2);
-	out = ft_calloc((str1len + str2len + 1), sizeof(char));
+	if (!str1 || !str2)
+		return (0);
+	str1len = gnl_strlen(str1);
+	str2len = gnl_strlen(str2);
+	out = malloc((str1len + str2len + 1) * sizeof(char));
 	if (!out)
-		return (NULL);
-	ft_strlcpy(out, str1, str1len + 1);
+		return (0);
+	gnl_strlcpy(out, str1, str1len + 1);
 	free(str1);
-	ft_strlcpy(out + str1len, str2, str2len + 1);
+	gnl_strlcpy(out + str1len, str2, str2len + 1);
 	return (out);
 }
 
-char	*ft_substr(char *str, uint32_t start, size_t len)
+char	*gnl_substr(char *str, uint32_t start, size_t len)
 {
 	char	*out;
 	size_t	slen;
 
-	slen = ft_strlen(str);
+	slen = gnl_strlen(str);
 	if (!str)
 		return (NULL);
 	if (start >= slen)
-		return (ft_strdup(""));
+		return (gnl_strdup(""));
 	if (len > slen - start)
-		return (ft_strdup(str + start));
+		return (gnl_strdup(str + start));
 	out = malloc((len + 1) * sizeof(char));
 	if (!out)
 		return (NULL);
-	ft_strlcpy(out, str + start, len);
+	gnl_strlcpy(out, str + start, len);
 	free(str);
 	out[len] = '\0';
 	return (out);
